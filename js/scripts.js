39e677513f7402a3798c59be663c21a27caf305f
LaTaevia <lataevia.berry@gmail.com>
2016-04-06 15:10:10 -0700

    update existing js troubleshooting for assigning an x or o to a user name; currently assigns properly but duplicates userinputted name on both player objects

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function Player(playerName, playerLetter){
 this.playerName = playerName;
 this.playerLetter = playerLetter;
 }
-
+var inputtedPlayerNames = []
 // scrap code
 function TicTacToe() {
   this.board = new TicTacToeBoard();
@@ -20,29 +20,39 @@ $(document).ready(function(){
 
   // Submit listener on form
   $('form#sign-up').submit(function(event){
-
+    event.preventDefault();
     // Both player objects via Player constructor
     $(".new-player-input").each(function() {
-      var inputtedPlayerNames = $(this).find("input#player-name").val();
-      var playerLetter = $(this).find("input.new-city").val();
-
+      inputtedPlayerNames.push($(this).find("input#player-name").val());
+      console.log(inputtedPlayerNames[1]);
       // Assign X or Y value to each player
-      for (i=0, i<2, i++){
+    for (var i=0; i<2; i++) {
+
         if (i===0) {
-          var player1 = new Player(inputtedPlayerNames, "X");
+          var player1 = new Player(inputtedPlayerNames[0], "X");
         }
-        else if (i===1){
-          var player2 = new Player(inputtedPlayerNames, "O");
+        else if (i===1) {
+          var player2 = new Player(inputtedPlayerNames[2], "O");
         }
+        else{
+          console.log("break");
+        }
+        debugger;
+        console.log(player1);
+        // return player1;
+        // return player2;
       }
     });
-    event.preventDefault();
+
   });
+
   // Click event listner on td, prevent default
   $('td').click(function(event){
     event.preventDefault();
     console.log("clicked");
   });
+
+
 });
 
 
